1ed8f18686051055ddf032aea38c6e6be2be5428
Indra Mahkota <indramahkota1@gmail.com>
2025-01-28 11:27:46 +0700

    Initial Commit

---
+++ CoffeeApp/CoffeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppApp.swift
+//  CoffeeApp
+//
+//  Created by Indra Mahkota on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeAppTests/CoffeeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppTests.swift
+//  CoffeeAppTests
+//
+//  Created by Indra Mahkota on 28/01/25.
+//
+
+import Testing
+@testable import CoffeeApp
+
+struct CoffeeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
